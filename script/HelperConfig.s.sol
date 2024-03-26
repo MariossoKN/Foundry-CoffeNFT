83ac7c972813d1c333ebd533ceb1b8762edfb538
MariossoKN <mariosso@Mariosso>
2024-03-26 18:58:42 +0100

    updated HelperConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -10,10 +10,26 @@ import {LinkToken} from "../test/mock/LinkToken.sol";
 contract HelperConfig is Script {
     CoffeNFT public coffeNFT;
     NetworkConfig public activeNetworkConfig;
+    VRFCoordinatorV2Mock public vrfCoordinatorV2Mock;
+    uint96 BASE_FEE = 0.25 ether;
+    uint96 GAS_PRICE_LINK = 1e9;
+    LinkToken public linkToken;
+    uint256 public constant DEFAULT_ANVIL_PRIVATE_KEY =
+        0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     struct NetworkConfig {
         address vrfCoordinator;
+        bytes32 gasLane;
+        uint64 subId;
+        uint16 requestConfirmations;
+        uint32 callbackGasLimit;
+        uint256 mintPrice;
+        uint256 totalSupply;
+        uint256 reservedSupply;
+        uint256 maxMintAmount;
+        string tokenUri;
         address link;
+        uint256 deployerKey;
     }
 
     constructor() {
@@ -26,21 +42,58 @@ contract HelperConfig is Script {
         }
     }
 
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory sepoliaNetworkConfig = NetworkConfig({
-            vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
-            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
-        });
-        return sepoliaNetworkConfig;
-    }
-
-    function getMainNetEthConfig() public pure returns (NetworkConfig memory) {
+    function getMainNetEthConfig() public view returns (NetworkConfig memory) {
         NetworkConfig memory mainNetNetworkConfig = NetworkConfig({
             vrfCoordinator: 0x271682DEB8C4E0901D1a1550aD2e64D568E69909,
-            link: 0x514910771AF9Ca656af840dff83E8264EcF986CA
+            gasLane: 0x9fe0eebf5e446e3c998ec9bb19951541aee00bb90ea201ae456421a2ded86805,
+            subId: 0,
+            requestConfirmations: 3,
+            callbackGasLimit: 500000,
+            mintPrice: 0.1 ether,
+            totalSupply: 10000000000000000000000,
+            reservedSupply: 150000000000000000000,
+            maxMintAmount: 5,
+            tokenUri: "ipfs://",
+            link: 0x514910771AF9Ca656af840dff83E8264EcF986CA,
+            deployerKey: vm.envUint("PRIVATE_KEY")
         });
         return mainNetNetworkConfig;
     }
 
-    function getOrCreateAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaNetworkConfig = NetworkConfig({
+            vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+            gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+            subId: 0,
+            requestConfirmations: 3,
+            callbackGasLimit: 500000,
+            mintPrice: 0.1 ether,
+            totalSupply: 10000000000000000000000,
+            reservedSupply: 150000000000000000000,
+            maxMintAmount: 5,
+            tokenUri: "ipfs://",
+            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+        return sepoliaNetworkConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        vrfCoordinatorV2Mock = new VRFCoordinatorV2Mock(BASE_FEE, GAS_PRICE_LINK);
+        NetworkConfig memory anvilNetworkConfig = NetworkConfig({
+            vrfCoordinator: address(vrfCoordinatorV2Mock),
+            gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+            subId: 0,
+            requestConfirmations: 3,
+            callbackGasLimit: 500000,
+            mintPrice: 0.1 ether,
+            totalSupply: 10000000000000000000000,
+            reservedSupply: 150000000000000000000,
+            maxMintAmount: 5,
+            tokenUri: "ipfs://",
+            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+            deployerKey: DEFAULT_ANVIL_PRIVATE_KEY
+        });
+        return anvilNetworkConfig;
+    }
 }
